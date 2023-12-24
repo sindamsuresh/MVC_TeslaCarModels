914f9526f107586e29a5e5d5a13d7058d037e3b5
SureshSindam <sindam.suresh@gmail.com>
2023-12-23 18:55:10 -0600

    Initial Commit

---
+++ MVC_TeslaCarModels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_TeslaCarModels
+//
+//  Created by Suresh Sindam on 12/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
