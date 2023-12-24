941b3361beff3a3a93efc5dfd6257224382a4970
SureshSindam <sindam.suresh@gmail.com>
2023-12-23 19:30:57 -0600

    initial folder set up

---
+++ MVC_TeslaCarModels/View/cells/CarModelCell.swift
@@ -0,0 +1,14 @@
+//
+//  CarModelCell.swift
+//  MVC_TeslaCarModels
+//
+//  Created by Suresh Sindam on 12/23/23.
+//
+
+import UIKit
+
+class CarModelCell: UICollectionViewCell {
+    
+    
+    
+}
